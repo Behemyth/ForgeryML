d9b64629c9dad81e5c2643719da9b64de2aeee31
zyteka <zyteka@rpi.edu>
2016-04-18 13:52:09 -0400

    Open file and input directory

--- backend.py
+++
@@ -1,7 +0,0 @@
-from .io import IOclass
-
-def main():
-	newIO = io.IO()
-
-
-if __name__ == '__main__': main()
--- finalproject.py
+++ finalproject.py
@@ -4,16 +4,41 @@
 #													  #
 #												      #
 #-----------------------------------------------------#
+
 import os
+import random
 
 class IOclass:
 	def __init__(self):
-		self.authorName = raw_input("ENTER AUTHOR NAME:   ")
-		self.filename = raw_input("ENTER FILENAME FOR PASSAGE:   ")
-		self.authorsWorks = raw_input("ENTER DIRECTORY NAME FOR KNOWN WORKS ('none' to skip):   ")
+
+		self.authorName = raw_input("ENTER AUTHOR NAME: ")
+		self.filename = raw_input("ENTER FILENAME FOR PASSAGE: ")
+		self.authorsWorks = raw_input("ENTER DIRECTORY NAME FOR KNOWN WORKS ('none' to skip): ")
 
 
-io = IOclass()
+def main():
+	#Prompt user for file and directory name
+	io = IOclass()
+
+	#open input file
+	filePath = "input/" + io.filename
+	inputFile = open(filePath, "r")
+
+	#FOR DEBUGGING (print input file data) ----------------------------------------
+	for line in inputFile:
+		print line
+	#------------------------------------------------------------------------------
+	dir = os.path.dirname(os.path.realpath(__import__("__main__").__file__))
+	path = dir + '/input/'
+	path = path + io.authorsWorks + '/'
+
+	for filename in os.listdir(path):
+		thisFile = open(path + filename, "r")
+		for line in thisFile:
+			print line
+
+if __name__ == '__main__': main()
+
 
 
 
