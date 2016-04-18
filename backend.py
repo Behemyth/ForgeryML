f8ac3a6208c9433b91e350263e3680d5a8581ec2
zyteka <zyteka@rpi.edu>
2016-04-18 13:18:30 -0400

    Basic IO stuff

---
+++ backend.py
@@ -0,0 +1,7 @@
+from .io import IOclass
+
+def main():
+	newIO = io.IO()
+
+
+if __name__ == '__main__': main()
---
+++ finalproject.py
@@ -0,0 +1,20 @@
+#-----------------------------------------------------#
+#				AI Final Project					  #
+#					Front End                         #
+#													  #
+#												      #
+#-----------------------------------------------------#
+import os
+
+class IOclass:
+	def __init__(self):
+		self.authorName = raw_input("ENTER AUTHOR NAME:   ")
+		self.filename = raw_input("ENTER FILENAME FOR PASSAGE:   ")
+		self.authorsWorks = raw_input("ENTER DIRECTORY NAME FOR KNOWN WORKS ('none' to skip):   ")
+
+
+io = IOclass()
+
+
+
+	
