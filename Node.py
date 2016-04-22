ff78d2fafe25ae63f2ac8aeeeb571c371cc20b22
Behemyth <asher.norland@gmail.com>
2016-04-22 13:32:05 -0400

    Added network

--- Node.py
+++ Node.py
@@ -1,7 +1,19 @@
-﻿class Node(object):
-    """description of class"""
-    def __init__(self):
-        print "Node"
+﻿import math
+import random
 
-    
-x=5
+BIAS = -1
+
+class Node(object):
+    def __init__(self, n_inputs ):
+        self.n_inputs = n_inputs
+        self.set_weights( [random.uniform(0,1) for x in range(0,n_inputs+1)] ) # +1 for bias weight
+
+    def sum(self, inputs ):
+        # Does not include the bias
+        return sum(val*self.weights[i] for i,val in enumerate(inputs))
+
+    def set_weights(self, weights ):
+        self.weights = weights
+
+    def __str__(self):
+        return 'Weights: %s, Bias: %s' % ( str(self.weights[:-1]),str(self.weights[-1]) )
