3b21c0663a1107a2e82236430d0f51eb77d83216
zyteka <zyteka@rpi.edu>
2016-04-24 16:06:46 -0400

    Added some error checking and reformatted main

--- finalproject.py
+++ finalproject.py
@@ -9,6 +9,7 @@ import os
 import random
 import shutil
 import Network
+from features import vector
 
 class IOclass:
 	def __init__(self):
@@ -24,21 +25,14 @@ def authorToVector(author):
 	for filename in os.listdir(path):
 		thisFile = open(path + filename, "r")
 
-def main():
-	#Prompt user for file and directory name
-	io = IOclass()
-
-	#open input file
-	filePath = "input/" + io.filename
-	inputFile = open(filePath, "r")
-
-	#FOR DEBUGGING (print input file data) ----------------------------------------
-	for line in inputFile:
-		print line
-	#------------------------------------------------------------------------------
+def movePassagesToDirectory(io):
 	dir = os.path.dirname(os.path.realpath(__import__("__main__").__file__))
 	path = dir + '/input/'
 	path = path + io.authorsWorks + '/'
+
+	if not os.path.exists(path):
+		print "ERROR: desired directory does not exist"
+		return
 
 	if not os.path.exists(dir + "/bin/database/" + io.authorName):
 		os.makedirs(dir + "/bin/database/" + io.authorName)
@@ -52,6 +46,26 @@ def main():
 			# COPY INPUT DIRECTORY INTO DATABASE --------------------------
 			shutil.copy(path + filename, dir + "/bin/database/" + io.authorName + "/")
 			# -------------------------------------------------------------
+
+def main():
+	#Prompt user for file and directory name
+	io = IOclass()
+
+	#open input file
+	filePath = "input/" + io.filename
+	inputFile = open(filePath, "r")
+
+	#FOR DEBUGGING (print input file data) ----------------------------------------
+	#for line in inputFile:
+	#	print line
+	#------------------------------------------------------------------------------
+
+	if io.authorsWorks.lower() is 'none':
+		print "no directory selected"
+	else:
+		movePassagesToDirectory(io)
+
+		
 
     #Network startup idk what the heck it takes---------------------------------------------------------
     #net=Network(,,,)
