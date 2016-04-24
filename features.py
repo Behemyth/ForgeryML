61a181b17c2d47dcaa2b361fa088380e7997e7ba
zyteka <zyteka@rpi.edu>
2016-04-24 15:54:16 -0400

    Changed file locatons

--- features.py
+++ features.py
@@ -1,14 +1,20 @@
 import os 
 
+def vector(filename, authorsName):
+	inputFile = open(filename, "r")
+	features = open(authorsName, "w")
+	features.write("<0,0,0>" + "\n")
+
+
 def main():
 	dir = os.path.dirname(os.path.realpath(__import__("__main__").__file__))
 	path = dir + "/bin/database/"
 	for author in os.listdir(path):
 		dirPath = path + "/" + author + "/"
-		features = open(path + author + ".txt", "w")
+		authorFile = dir + "/bin/" + author + ".txt"
 		for filename in os.listdir(dirPath):
 			print filename
-			inputFile = open(dirPath + filename, "r")
-			features.write("<0,0,0>" + "\n")
+			featuresFile = dirPath + filename
+			vector(featuresFile, authorFile)
 
 if __name__ == '__main__': main()
