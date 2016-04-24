a2f54c0ca9fd739eee2102388744e188d2e0a897
Behemyth <asher.norland@gmail.com>
2016-04-24 16:20:50 -0400

    I think I did something

--- Network.py
+++ Network.py
@@ -1,75 +1,160 @@
-﻿class NeuralNetwork:
-    def __init__(self, n_inputs, n_outputs, n_neurons_to_hl, n_hidden_layers):
-            self.n_inputs = n_inputs
-            self.n_outputs = n_outputs
-            self.n_hidden_layers = n_hidden_layers
-            self.n_neurons_to_hl = n_neurons_to_hl
+﻿import math
+import random
+import string
+import random
+from datetime import datetime
 
-            # Do not touch
-            self._create_network()
-            self._n_weights = None
-            # end
+random.seed(datetime.now())
 
-    def _create_network(self):
-            if self.n_hidden_layers > 0:
-                # create the first layer
-                self.layers = [NeuronLayer(self.n_neurons_to_hl,self.n_inputs)]
+def GetRandom(a, b):
+    return (b - a) * random.random() + a
 
-                # create hidden layers
-                self.layers += [NeuronLayer(self.n_neurons_to_hl,self.n_neurons_to_hl) for _ in range(0,self.n_hidden_layers)]
+def Matrix(I, J, fill=0.0):
+    m = []
+    for i in range(I):
+        m.append([fill] * J)
+    return m
 
-                # hidden-to-output layer
-                self.layers += [NeuronLayer(self.n_outputs,self.n_neurons_to_hl)]
-            else:
-                # If we don't require hidden layers
-                self.layers = [NeuronLayer(self.n_outputs,self.n_inputs)]
+def Sigmoid(x):
+    return math.tanh(x)
 
-    def get_weights(self):
-            weights = []
+def SigmoidDerivative(x):
+    return 1.0 - x ** 2
 
-            for layer in self.layers:
-                for neuron in layer.neurons:
-                    weights += neuron.weights
+class Network:
+    def __init__(self, numberInput, numberHidden, numberOutput):
 
-            return weights
+        self.numberInput = numberInput + 1 
+        self.numberHidden = numberHidden
+        self.numberOutput = numberOutput
 
-    @property
-    def n_weights(self):
-            if not self._n_weights:
-                self._n_weights = 0
-                for layer in self.layers:
-                    for neuron in layer.neurons:
-                        self._n_weights += neuron.n_inputs + 1 # +1 for bias weight
-            return self._n_weights
+        self.activationInput = [1.0] * self.numberInput
+        self.activationHidden = [1.0] * self.numberHidden
+        self.activationOutput = [1.0] * self.numberOutput
+        
+        self.weightsInput = makeMatrix(self.numberInput, self.numberHidden)
+        self.weightsOutput = makeMatrix(self.numberHidden, self.numberOutput)
 
-    def set_weights(self, weights):
-            assert len(weights) == self.n_weights, "Incorrect amount of weights."
 
-            stop = 0
-            for layer in self.layers:
-                for neuron in layer.neurons:
-                    start, stop = stop, stop + (neuron.n_inputs + 1)
-                    neuron.set_weights(weights[start:stop])
-            return self
+        for i in range(self.numberInput):
+            for j in range(self.numberHidden):
+                self.weightsInput[i][j] = rand(-0.2, 0.2)
 
-    def update(self, inputs):
-            assert len(inputs) == self.n_inputs, "Incorrect amount of inputs."
+        for j in range(self.numberHidden):
+            for k in range(self.numberOutput):
+                self.weightsOutput[j][k] = rand(-2.0, 2.0)
 
-            for layer in self.layers:
-                outputs = []
-                for neuron in layer.neurons:
-                    tot = neuron.sum(inputs) + neuron.weights[-1] * BIAS
-                    outputs.append(self.sigmoid(tot))
-                inputs = outputs   
-            return outputs
+        self.ci = makeMatrix(self.numberInput, self.numberHidden)
+        self.co = makeMatrix(self.numberHidden, self.numberOutput)
 
-    def sigmoid(self, activation,response=1):
-            # the activation function
-            try:
-                return 1 / (1 + math.e ** (-activation / response))
-            except OverflowError:
-                return float("inf")
 
-    def __str__(self):
-            return '\n'.join([str(i + 1) + ' ' + str(layer) for i,layer in enumerate(self.layers)])
 
+
+
+
+    def Update(self, inputs):
+
+        if len(inputs) != self.numberInput - 1:
+            raise ValueError('Improper input size in network function: Update')
+
+        for i in range(self.numberInput - 1):
+            self.activationInput[i] = inputs[i]
+
+        for j in range(self.numberHidden):
+            sum = 0.0
+            for i in range(self.numberInput):
+                sum = sum + self.activationInput[i] * self.weightsInput[i][j]
+            self.activationHidden[j] = Sigmoid(sum)
+
+        for k in range(self.numberOutput):
+            sum = 0.0
+            for j in range(self.numberHidden):
+                sum = sum + self.activationHidden[j] * self.weightsOutput[j][k]
+            self.activationOutput[k] = Sigmoid(sum)
+
+        return self.activationOutput[:]
+
+
+
+
+
+
+    def BackPropagation(self, targetNodes, learningRate, momentum):
+        if len(targetNodes) != self.numberOutput:
+            raise ValueError('Improper target size in network function: BackPropagation')
+
+        outputDeltaErrors = [0.0] * self.numberOutput
+        for k in range(self.numberOutput):
+            error = targetNodes[k] - self.activationOutput[k]
+            outputDeltaErrors[k] = SigmoidDerivative(self.activationOutput[k]) * error
+
+        hiddenDeltaErrors = [0.0] * self.numberHidden
+        for j in range(self.numberHidden):
+            error = 0.0
+            for k in range(self.numberOutput):
+                error = error + outputDeltaErrors[k] * self.weightsOutput[j][k]
+            hiddenDeltaErrors[j] = SigmoidDerivative(self.activationHidden[j]) * error
+
+
+
+
+        for j in range(self.numberHidden):
+            for k in range(self.numberOutput):
+                change = outputDeltaErrors[k] * self.activationHidden[j]
+                self.weightsOutput[j][k] = self.weightsOutput[j][k] + learningRate * change + momentum * self.co[j][k]
+                self.co[j][k] = change
+
+        for i in range(self.numberInput):
+            for j in range(self.numberHidden):
+                change = hiddenDeltaErrors[j] * self.activationInput[i]
+                self.weightsInput[i][j] = self.weightsInput[i][j] + learningRate * change + momentum * self.ci[i][j]
+                self.ci[i][j] = change
+
+
+
+        error = 0.0
+        for k in range(len(targetNodes)):
+            error = error + 0.5 * (targetNodes[k] - self.activationOutput[k]) ** 2
+        return error
+
+
+
+
+
+
+
+    def Test(self, inputSet):
+        for p in inputSet:
+            print(p[0], '->', self.Update(p[0]))
+
+
+
+
+
+
+    def PrintWeights(self):
+        print('Input weights:')
+        for i in range(self.numberInput):
+            print(self.weightsInput[i])
+        print()
+        print('Output weights:')
+        for j in range(self.numberHidden):
+            print(self.weightsOutput[j])
+
+
+
+
+
+
+
+
+    def Train(self, inputSet, iterations=100, learningRate=0.5, momentum=0.1):
+
+        for i in range(iterations):
+            error = 0.0
+            for p in inputSet:
+                inputs = p[0]
+                targetNodes = p[1]
+                self.Update(inputs)
+                error = error + self.BackPropagation(targetNodes, learningRate, momentum)
+                print('error %-.5f' % error)
